0cd603af18bd9d65c693582f335cfc44f9e6b33a
nithishreddy27 <nithishreddy0627@gmail.com>
2023-01-23 18:02:10 +0530

    s

---
+++ pages/api/signup.js
@@ -0,0 +1,12 @@
+import { createUser } from '../../lib/user'
+
+export default async function signup(req, res) {
+  try {
+    
+
+    
+  } catch (error) {
+    console.error(error)
+    res.status(500).end(error.message)
+  }
+}
